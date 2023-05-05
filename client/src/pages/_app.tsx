f165e47d6d7ae461c715579e92481a82fdb94688
Hong Nguyen <nguyenhuynhthuhong@gmail.com>
2023-05-05 11:45:35 +0700

    chore: set up SSR with apollo client

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,17 +2,15 @@
 import React from 'react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '@/lib/apolloClient'
 
-const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL as string,
-  credentials: 'include',
-  cache: new InMemoryCache(),
-})
 
 export default function App({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps)
+
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apolloClient}>
       <Component {...pageProps} />
     </ApolloProvider>
   )
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,28 @@
 import React from "react"
 import { Navbar } from "@/components"
+import { addApolloState, initializeApollo } from "@/lib/apolloClient"
+import { PostsDocument, usePostsQuery } from "@/generated/graphql"
 
 export default function Home() {
+  const { data } = usePostsQuery()
   return <>
     <Navbar />
     <div>Hello World</div>
+    <ul>
+      {data?.posts.map(post => <li>{post.title}</li>)}
+    </ul>
   </>
+}
+
+
+export const getStaticProps = async () => {
+  const apolloClient = initializeApollo()
+
+  await apolloClient.query({
+    query: PostsDocument
+  })
+
+  return addApolloState(apolloClient, {
+    props: {}
+  })
 }
