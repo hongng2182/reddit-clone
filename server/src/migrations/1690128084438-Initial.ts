7373cfb3ca434ec473e3999332aba3adc312ac06
Hong Nguyen <nguyenhuynhthuhong@gmail.com>
2023-07-23 23:31:46 +0700

    fix: build error

--- server/src/migrations/1690128084438-Initial.ts
+++ server/src/migrations/1690128084438-Initial.ts
@@ -2,10 +2,10 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class Initial1690128084438 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
+    public async up(_queryRunner: QueryRunner): Promise<void> {
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
+    public async down(_queryRunner: QueryRunner): Promise<void> {
     }
 
 }
